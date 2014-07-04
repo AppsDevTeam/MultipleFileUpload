569e00139b7378310a6329ca6c352345413cfaa7
Michal Lohnisky <michal@appsdevteam.com>
2014-07-04 12:53:55 +0200

    Resumable: funkce _log

--- MultipleFileUpload/UI/Resumable/Controller.php
+++ MultipleFileUpload/UI/Resumable/Controller.php
@@ -519,13 +519,15 @@ class Controller extends AbstractInterface {
 
 			// log to the output
 			$log_str = date('d.m.Y').": {$str}\r\n";
-			echo $log_str;
+			//echo $log_str;
 
+			/*
 			// log to file
 			if (($fp = fopen('/home/michal/www/zenskecykly.cz/private/temp/upload_log.txt', 'a+')) !== false) {
 					fputs($fp, $log_str);
 					fclose($fp);
 			}
+			*/
 	}
 
 	/**
